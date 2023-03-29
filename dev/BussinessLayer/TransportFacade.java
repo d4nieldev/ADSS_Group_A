062e014d988133b462e5898fbdc6eedae55bfa38
kfirShal <kfirshal@post.bgu.ac.il>
2023-03-29 19:57:18 +0300

    Update TransportFacade.java
    new

--- dev/BussinessLayer/TransportFacade.java
+++ dev/BussinessLayer/TransportFacade.java
@@ -1,4 +1,28 @@
 package BussinessLayer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TransportFacade {
+    private Map<String, Transport> transportMap;
+
+    public TransportFacade() {
+        transportMap = new HashMap<>();
+    }
+
+    public void addTransport(String name, Transport transport) {
+        transportMap.put(name, transport);
+    }
+
+    public Transport getTransport(String name) {
+        return transportMap.get(name);
+    }
+
+    public void removeTransport(String name) {
+        transportMap.remove(name);
+    }
+
+    public Map<String, Transport> getAllTransport() {
+        return transportMap;
+    }
 }

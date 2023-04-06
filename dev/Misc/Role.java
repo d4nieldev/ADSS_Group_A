72fa52a4d774d8650bd9c6fbe0f6ae0054640337
inbarbenchaim <117454266+inbarbenchaim@users.noreply.github.com>
2023-04-06 11:34:14 +0300

    change role from enum to class

--- dev/Misc/Role.java
+++ dev/Misc/Role.java
@@ -1,5 +1,16 @@
 package Misc;
 
-public enum Role {
-    HRMANAGER, BRANCHMANAGER, SHIFTMANAGER, CHASHIER, STOREKEEPER, DRIVER, GENERRAL, CLEANER, SECURITY
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Role {
+
+    public final List<String> listRoles = Arrays.asList(
+        "HRMANAGER", "BRANCHMANAGER", "SHIFTMANAGER", "CHASHIER", "STOREKEEPER", "DRIVER", "GENERRAL", "CLEANER", "SECURITY");
+
+    public void addRole(String role){
+        if(listRoles.contains(role)){throw new Error("This role is already exsist in the system.");}
+        listRoles.add(role);
+    }
 }

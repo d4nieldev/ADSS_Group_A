ca80bdea33049fbc75af02f374453bdd810746ef
TalKoren11 <kotal@post.bgu.ac.il>
2023-04-09 16:25:28 +0300

    Added empty tests.

---
+++ dev/Tests/EmployeeControllerTest.java
@@ -0,0 +1,136 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void logIn() {
+    }
+
+    @Test
+    void logOut() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void printAllEmployees() {
+    }
+
+    @Test
+    void checkShiftInDate() {
+    }
+
+    @Test
+    void addRoleToEmployee() {
+    }
+
+    @Test
+    void removeRoleFromEmployee() {
+    }
+
+    @Test
+    void checkRoleInEmployee() {
+    }
+
+    @Test
+    void addBonus() {
+    }
+
+    @Test
+    void addRoleToSystem() {
+    }
+
+    @Test
+    void addPremissionRole() {
+    }
+
+    @Test
+    void removePremissionRole() {
+    }
+
+    @Test
+    void getAllDrivers() {
+    }
+
+    @Test
+    void changeFirstName() {
+    }
+
+    @Test
+    void changeLastName() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void changeBankNum() {
+    }
+
+    @Test
+    void changeBankBranch() {
+    }
+
+    @Test
+    void changeBankAccount() {
+    }
+
+    @Test
+    void changeSalary() {
+    }
+
+    @Test
+    void changeStartDate() {
+    }
+
+    @Test
+    void changeDriverLicence() {
+    }
+
+    @Test
+    void getAddCancelationListAccess() {
+    }
+
+    @Test
+    void getPrintFinalShiftListAccess() {
+    }
+
+    @Test
+    void getMissingStaffToRoleListAccess() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void checkLoggedIn() {
+    }
+
+    @Test
+    void checkHrManager() {
+    }
+
+    @Test
+    void checkEmployee() {
+    }
+
+    @Test
+    void checkIfEmployeeAllowed() {
+    }
+}

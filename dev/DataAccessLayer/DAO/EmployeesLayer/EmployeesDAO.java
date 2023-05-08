946bc461cb71ce7e709ee962776642b5eeed89c9
TalKoren11 <kotal@post.bgu.ac.il>
2023-05-08 20:53:06 +0300

    fixed bugs in repository.

--- dev/DataAccessLayer/DAO/EmployeesLayer/EmployeesDAO.java
+++ dev/DataAccessLayer/DAO/EmployeesLayer/EmployeesDAO.java
@@ -20,14 +20,15 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
     public int insert(EmployeeDTO Ob) {
         int ans = 0;
         Connection conn = Repository.getInstance().connect();
-        if (Ob == null) return 0;
+        if (Ob == null)
+            return 0;
         String toInsertEmp = Ob.fieldsToString();
         Statement s;
         try {
             s = conn.createStatement();
             s.executeUpdate(InsertStatement(toInsertEmp));
             int resES = insertToEmployeeRoles(Ob);
-            if (resES == 1) //If inserts worked
+            if (resES == 1) // If inserts worked
                 ans = 1;
             else {
                 ans = 0;
@@ -42,7 +43,8 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
 
     private int insertToEmployeeRoles(EmployeeDTO Ob) {
         Connection conn = Repository.getInstance().connect();
-        if (Ob == null) return 0;
+        if (Ob == null)
+            return 0;
         for (int index = 0; index < Ob.getNumberOfRoles(); index++) {
             String toInsertEmployeeRole = String.format("INSERT INTO %s \n" +
                     "VALUES %s;", "EmployeesRoles", Ob.getRole(index));
@@ -58,23 +60,40 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
     }
 
     @Override
-    public int update(EmployeeDTO updatedOb) { //not allowed to change ID
-            Connection conn = Repository.getInstance().connect();
-            if (updatedOb == null) return 0;
-            String updateString = String.format("UPDATE %s" +
-                            " SET \"FirstName\"= \"%s\", \"LastName\"= \"%s\", \"Password\"= \"%s\", \"BankNumber\"= \"%s\" " +
-                            ", \"BankBranchNumber\"=\"%s\", \"BankAccountNumber\"=%s,  \"Salary\"=\"%s\", \"Bonus\"=\"%s\" " +
-                            ", \"startDate\"=\"%s\", \"TempsEmployment\"=%s,  \"IsLoggedIn\"=\"%s\", \"SuperBranch\"=\"%s\" " +
-                            "WHERE \"ID\" = \"%s\";",
-                tableName, updatedOb.firstName, updatedOb.lastName, updatedOb.password, updatedOb.bankNum, updatedOb.bankBranch, updatedOb.bankAccount, 
-                updatedOb.salary, updatedOb.bonus, updatedOb.startDate, updatedOb.tempsEmployment, updatedOb.isLoggedIn, updatedOb.superBranch);
-            Statement s;
-            try {
-                s = conn.createStatement();
-                return s.executeUpdate(updateString);
-            } catch (Exception e) {
-                return 0;
-            }
+    public int update(EmployeeDTO updatedOb) { // not allowed to change ID
+        Connection conn = Repository.getInstance().connect();
+        if (updatedOb == null)
+            return 0;
+        // String updateString = String.format("UPDATE %s" +
+        // " SET \"FirstName\"= \"%s\", \"LastName\"= \"%s\", \"Password\"= \"%s\",
+        // \"BankNumber\"= \"%s\" " +
+        // ", \"BankBranchNumber\"=\"%s\", \"BankAccountNumber\"=%s, \"Salary\"=\"%s\",
+        // \"Bonus\"=\"%s\" " +
+        // ", \"startDate\"=\"%s\", \"TempsEmployment\"=%s, \"IsLoggedIn\"=\"%s\",
+        // \"SuperBranch\"=\"%s\" " +
+        // "WHERE \"ID\" = \"%s\";",
+        // tableName, updatedOb.firstName, updatedOb.lastName, updatedOb.password,
+        // updatedOb.bankNum, updatedOb.bankBranch, updatedOb.bankAccount,
+        // updatedOb.salary, updatedOb.bonus, updatedOb.startDate,
+        // updatedOb.tempsEmployment, updatedOb.isLoggedIn, updatedOb.superBranch);
+
+        String updateString = String.format(
+                "UPDATE %s SET \"FirstName\" = \"%s\", \"LastName\" = \"%s\", \"Password\" = \"%s\", \"BankNumber\" = \"%s\", "
+                        +
+                        "\"BankBranchNumber\" = \"%s\", \"BankAccountNumber\" = %d, \"Salary\" = %f, \"Bonus\" = %f, \"startDate\" = \"%s\", "
+                        +
+                        "\"TempsEmployment\" = %b, \"IsLoggedIn\" = %b, \"SuperBranch\" = \"%s\" WHERE \"ID\" = \"%s\";",
+                tableName, updatedOb.firstName, updatedOb.lastName, updatedOb.password, updatedOb.bankNum,
+                updatedOb.bankBranch, updatedOb.bankAccount,
+                updatedOb.salary, updatedOb.bonus, updatedOb.startDate, updatedOb.tempsEmployment, updatedOb.isLoggedIn,
+                updatedOb.superBranch);
+        Statement s;
+        try {
+            s = conn.createStatement();
+            return s.executeUpdate(updateString);
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
@@ -87,13 +106,13 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
             if (roles == null) {
                 return null;
             }
-            output = new EmployeeDTO(/*Id*/RS.getInt(1), /*first name*/RS.getString(2),
-                /*last name*/RS.getString(3), /*password*/RS.getString(4),
-                    /*bank number*/RS.getInt(5), /*bank branch number*/RS.getInt(6), 
-                    /*bank account number*/RS.getInt(7), /*salary*/RS.getInt(8),
-                    /*bonus*/ RS.getInt(9), /*start date*/ LocalDate.parse(RS.getString(10)),
-                    /*temps employment*/ RS.getString(11), roles,
-                     /*is logged in*/ RS.getBoolean(13), /*super branch*/ RS.getInt(14));
+            output = new EmployeeDTO(/* Id */RS.getInt(1), /* first name */RS.getString(2),
+                    /* last name */RS.getString(3), /* password */RS.getString(4),
+                    /* bank number */RS.getInt(5), /* bank branch number */RS.getInt(6),
+                    /* bank account number */RS.getInt(7), /* salary */RS.getInt(8),
+                    /* bonus */ RS.getInt(9), /* start date */ LocalDate.parse(RS.getString(10)),
+                    /* temps employment */ RS.getString(11), roles,
+                    /* is logged in */ RS.getBoolean(13), /* super branch */ RS.getInt(14));
         } catch (Exception e) {
             output = null;
         } finally {
@@ -102,7 +121,7 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
         return output;
     }
 
-    public LinkedList<Integer> getRolesList(String id,Connection conn){
+    public LinkedList<Integer> getRolesList(String id, Connection conn) {
         LinkedList<Integer> ans = new LinkedList<>();
         ResultSet rs = get("EmployeesRoles", "EmployeeID", id, conn);
         try {
@@ -114,6 +133,7 @@ public class EmployeesDAO extends DAO<EmployeeDTO> {
         }
         return ans;
     }
+
     public int addRole(int empID, Integer roleToAdd) {
         return employeeRoleDAO.addRole(empID, roleToAdd);
     }

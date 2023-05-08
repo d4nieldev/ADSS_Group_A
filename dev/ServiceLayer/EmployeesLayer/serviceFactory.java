23afea601688a3235a0d7eea6f01c85044e94dd2
TalKoren11 <kotal@post.bgu.ac.il>
2023-05-08 11:40:07 +0300

    add transport to service factory and done with ,MainV2 for now.

--- dev/ServiceLayer/EmployeesLayer/serviceFactory.java
+++ dev/ServiceLayer/EmployeesLayer/serviceFactory.java
@@ -4,8 +4,12 @@ import BussinessLayer.EmployeeTransportFacade;
 import BussinessLayer.EmployeesLayer.BranchFacade;
 import BussinessLayer.EmployeesLayer.EmployeeFacade;
 import BussinessLayer.EmployeesLayer.ShiftFacade;
+import ServiceLayer.TransportLayer.DriverService;
+import ServiceLayer.TransportLayer.TransportService;
+import ServiceLayer.TransportLayer.TruckService;
 
 public class serviceFactory {
+    //////////HR/////////////////
     private EmployeeFacade employeeFacade;
     private ShiftFacade shiftFacade;
     private BranchFacade branchFacade;
@@ -13,8 +17,13 @@ public class serviceFactory {
     private EmployeeService employeeService;
     private ShiftService shiftService;
     private BranchService branchService;
+    /////////Transport/////////////
+    private TransportService transportServices;
+    private DriverService driverService;
+    private TruckService truckService;
 
     public serviceFactory(){
+        //////////HR/////////////////
         employeeFacade = new EmployeeFacade();
         shiftFacade = new ShiftFacade(employeeFacade);
         branchFacade = new BranchFacade(employeeFacade, shiftFacade);
@@ -22,12 +31,21 @@ public class serviceFactory {
         employeeService = new EmployeeService(employeeFacade, employeeTransportFacade);
         shiftService = new ShiftService(shiftFacade);
         branchService = new BranchService(branchFacade);
+        /////////Transport/////////////
+        transportServices = new TransportService();
+        driverService = new DriverService();
+        truckService = new TruckService();
     }
 
+    //////////HR/////////////////
     public EmployeeFacade getemployeeFacade(){return employeeFacade;}
     public ShiftFacade getshiftFacade(){return shiftFacade;}
     public BranchFacade getbranchFacade(){return branchFacade;}
     public EmployeeService getEmployeeService(){return employeeService;}
     public ShiftService getShiftService(){return shiftService;}
     public BranchService getBranchService(){return branchService;}
+    /////////Transport/////////////
+    public TransportService getTransportService(){return transportServices;};
+    public DriverService getDriverService(){return driverService;};
+    public TruckService getTruckService(){return truckService;};
 }

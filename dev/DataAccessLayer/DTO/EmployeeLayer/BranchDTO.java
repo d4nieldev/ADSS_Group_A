da0c9b838ccda533939900b5c73d0825d5138c4c
inbarbenchaim <117454266+inbarbenchaim@users.noreply.github.com>
2023-05-09 09:34:52 +0300

    start branchDAO and DTO

--- dev/DataAccessLayer/DTO/EmployeeLayer/BranchDTO.java
+++ dev/DataAccessLayer/DTO/EmployeeLayer/BranchDTO.java
@@ -28,5 +28,25 @@ public class BranchDTO {
         return String.format("(\"%d\",\"%s\",\"%s\")",
 		 this.branchId, this.address, this.location);
     }
+    
+	public int getNumberOfOriginEmployees(){return originEmployees.size();}
+    public String getOriginEmployee(int index) {
+        return String.format("(\"%d\",\"%d\",\"%s\")", this.branchId, originEmployees.get(index), "ORIGIN");
+    }
+
+	public int getNumberOfForeignEmployees(){return foreignEmployees.size();}
+    public String getForeignEmployee(int index) {
+        return String.format("(\"%d\",\"%d\",\"%s\")", this.branchId, foreignEmployees.get(index), "FOREIGN");
+    }
+
+	public int getNumberOfnotAllowEmployees(){return notAllowEmployees.size();}
+    public String getnotAllowEmployee(int index) {
+        return String.format("(\"%d\",\"%d\",\"%s\")", this.branchId, notAllowEmployees.get(index), "NOTALLOW");
+    }
+
+	public int getNumberOfShifts(){return shifts.size();}
+    public String getShift(int index) {
+        return String.format("(\"%d\",\"%d\")", this.branchId, shifts.get(index));
+    }
 
 }

531dd003b3a3379fb22d99aa565357a297a01ef6
TalKoren11 <kotal@post.bgu.ac.il>
2023-05-10 02:04:45 +0300

    minor things - syntax...

--- dev/PresentationLayer/MainV2.java
+++ dev/PresentationLayer/MainV2.java
@@ -19,15 +19,11 @@ class MainV2 {
     private static serviceFactory serviceFactory = new serviceFactory();
     private static HRSystem hrSystem;
     private static MemberSystem memberSystem;
-    //private static TransportSystem transportSystem;
-
-    private static TruckService truckService = new TruckService();
-    private static TransportService transportService = serviceFactory.getTransportService();
+    private static TransportSystem transportSystem;
 
     public static void main(String[] args) {
 
-        hrSystem = new HRSystem(serviceFactory);
-        memberSystem = new MemberSystem(serviceFactory);
+
 
         ////////////////My Beautifual Test Area/////////////////////
         //hrSystem.employeeService.logIn(123456789, "abc");
@@ -38,61 +34,63 @@ class MainV2 {
         //System.out.println("check");
         ////////////////My Beautifual Test Area/////////////////////
 
-        //transportSystem = new TransportSystem(serviceFactory);
+        hrSystem = new HRSystem(serviceFactory);
+        memberSystem = new MemberSystem(serviceFactory);
+        transportSystem = new TransportSystem(serviceFactory);
 
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Hello there, in order to login to the system please enter your Id: ");
-        // int loginId = Integer.parseInt(sc.nextLine());
-        // System.out.println("");
+        System.out.print("Hello there, in order to login to the system please enter your Id: ");
+        int loginId = Integer.parseInt(sc.nextLine());
+        System.out.println("");
 
-        // System.out.print("Great, now enter your password: ");
-        // String loginPassword = sc.nextLine();
-        // System.out.println("");
+        System.out.print("Great, now enter your password: ");
+        String loginPassword = sc.nextLine();
+        System.out.println("");
 
-        // while (true){
-        //     try {
-        //         hrSystem.employeeService.logIn(loginId, loginPassword);
-        //         System.out.println("");
-        //         break;
-        //     }
-        //     catch (Error e) {
-        //         System.out.println(e.toString());
-        //         System.out.println();
-        //         System.out.print("Enter your Id again: ");
-        //         loginId = Integer.parseInt(sc.nextLine());
-        //         System.out.println("");
+        while (true){
+            try {
+                hrSystem.employeeService.logIn(loginId, loginPassword);
+                System.out.println("");
+                break;
+            }
+            catch (Error e) {
+                System.out.println(e.toString());
+                System.out.println();
+                System.out.print("Enter your Id again: ");
+                loginId = Integer.parseInt(sc.nextLine());
+                System.out.println("");
 
-        //         System.out.print("Enter your password again: ");
-        //         loginPassword = sc.nextLine();
-        //         System.out.println("");
-        //     }
-        // }
+                System.out.print("Enter your password again: ");
+                loginPassword = sc.nextLine();
+                System.out.println("");
+            }
+        }
 
-        // System.out.println("Wellcome to the system\n");
+        System.out.println("Wellcome to the system\n");
         
-        // String managerRole = hrSystem.employeeService.getManagerType(loginId);
+        String managerRole = hrSystem.employeeService.getManagerType(loginId);
 
-        // switch (managerRole){
-        //     case("HRMANAGER") : {
+        switch (managerRole){
+            case("HRMANAGER") : {
 
-        //         hrSystem.run(loginId);
+                hrSystem.run(loginId);
                 
-        //         break;
-        //     }                
+                break;
+            }                
 
-        //     case("TRANSPORTMANAGER") : {
-        //         transportSystem.run(loginId);
-        //         break;
-        //     }
+            case("TRANSPORTMANAGER") : {
+                transportSystem.run(loginId);
+                break;
+            }
             
-        //     default : {
-        //         memberSystem.run(loginId);
-        //         break;
-        //     }
-        // }
+            default : {
+                memberSystem.run(loginId);
+                break;
+            }
+        }
 
-        // sc.close();
+        sc.close();
     }
 
 
--- dev/PresentationLayer/MemberSystem.java
+++ dev/PresentationLayer/MemberSystem.java
@@ -10,7 +10,7 @@ import ServiceLayer.EmployeesLayer.ShiftService;
 import ServiceLayer.EmployeesLayer.serviceFactory;
 
 public class MemberSystem {
-    
+
     EmployeeService employeeService;
     ShiftService shiftService;
     BranchService branchService;
@@ -26,9 +26,9 @@ public class MemberSystem {
         System.out.println("1 - Show all shifts.\n 2 - Add constraint for a shift.\n3 - Go back.");
         String option = sc.nextLine();
 
-        while(!option.equals("0")){
-            try{
-                if (option.equals("1")){ // 1 Show all shifts
+        while (!option.equals("0")) {
+            try {
+                if (option.equals("1")) { // 1 Show all shifts
                     System.out.println("Please enter the date that you would like to work in.\n");
                     System.out.println("Please enster the date in that format Date: 05-06-2003\n");
                     String date = sc.nextLine();
@@ -36,11 +36,11 @@ public class MemberSystem {
 
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
                     LocalDate localDate = LocalDate.parse(date, formatter);
-                    
+
                     branchService.printAvailableShiftForEmployee(loginId, localDate);
                 }
 
-                else if(option.equals("2")){ // 2 Add constraint for a shift
+                else if (option.equals("2")) { // 2 Add constraint for a shift
                     System.out.println("You choose to add constraint for an employee to a shift.");
                     System.out.println("please enter the following information:");
                     System.out.println("");
@@ -61,9 +61,8 @@ public class MemberSystem {
                 }
 
                 System.out.println("");
-                System.out.println("1 - Show all shifts.\n 2 - Add constraint for a shift.\n3 - Go back.");
-            }
-            catch(Error e) {
+                System.out.println("1 - Show all shifts.\n2 - Add constraint for a shift.\n3 - Go back.");
+            } catch (Error e) {
                 System.out.println(e.toString());
                 System.out.print("Please enter AGAIN your request to the system according to the PDF file: ");
                 option = sc.nextLine();

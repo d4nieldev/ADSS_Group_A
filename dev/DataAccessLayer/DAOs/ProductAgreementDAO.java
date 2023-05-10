b23f0a4aa73a85689404f0d1061b5618e312effd
vladik1998 <shembel.vladik@gmail.com>
2023-05-11 00:24:12 +0300

    added ProductAgreement dao and dto

---
+++ dev/DataAccessLayer/DAOs/ProductAgreementDAO.java
@@ -0,0 +1,32 @@
+package DataAccessLayer.DAOs;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DataAccessLayer.DTOs.ProductAgreementDTO;
+
+public class ProductAgreementDAO extends DAO<ProductAgreementDTO> {
+
+    private ProductsDAO productDAO;
+
+    public ProductAgreementDAO() {
+        super("ProductAgreement");
+        productDAO = ProductsDAO.getInstance();
+    }
+
+    @Override
+    public ProductAgreementDTO makeDTO(ResultSet rs) throws SQLException {
+        if (!rs.next())
+            throw new SQLException("Can't make DTO from nothing!");
+
+        int supplierId = rs.getInt("supplierId");
+        int productId = rs.getInt("productId");
+        int stockAmount = rs.getInt("stockAmount");
+        double minQuantity = rs.getDouble("basePrice");
+        int productSupplierId = rs.getInt("productSupplierId");
+
+        return new ProductAgreementDTO(supplierId, productDAO.getById(productId), stockAmount, minQuantity,
+                productSupplierId, );
+    }
+
+}
--- dev/DataAccessLayer/DTOs/ProductAgreementDTO.java
+++ dev/DataAccessLayer/DTOs/ProductAgreementDTO.java
@@ -1,5 +1,37 @@
 package DataAccessLayer.DTOs;
 
-public class ProductAgreementDTO {
-    
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProductAgreementDTO implements DTO {
+
+    private int supplierId;
+    private ProductDTO productDTO;
+    private int stockAmount;
+    private double basePrice;
+    private int productSupplierId;
+    private TreeMap<Integer, DiscountDTO> amountToDiscount;
+
+    public ProductAgreementDTO(int supplierId, ProductDTO productDTO, int stockAmount, double basePrice,
+            int productSupplierId, TreeMap<Integer, DiscountDTO> amountToDiscount) {
+        this.supplierId = supplierId;
+        this.productDTO = productDTO;
+        this.stockAmount = stockAmount;
+        this.basePrice = basePrice;
+        this.productSupplierId = productSupplierId;
+        this.amountToDiscount = amountToDiscount;
+    }
+
+    @Override
+    public Map<String, String> getNameToVal() {
+        Map<String, String> nameToVal = new HashMap<>();
+        nameToVal.put("supplierId", "" + supplierId);
+        nameToVal.put("productId", "" + productDTO.getId());
+        nameToVal.put("stockAmount", "" + stockAmount);
+        nameToVal.put("basePrice", "" + basePrice);
+        nameToVal.put("productSupplierId", "" + productSupplierId);
+        return nameToVal;
+    }
+
 }

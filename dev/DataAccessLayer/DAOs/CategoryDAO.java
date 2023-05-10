9838d82fb2940a6b469043801b70b00c7aa52af9
vladik1998 <shembel.vladik@gmail.com>
2023-05-10 23:19:43 +0300

    Added category dto and dao

---
+++ dev/DataAccessLayer/DAOs/CategoryDAO.java
@@ -0,0 +1,45 @@
+package DataAccessLayer.DAOs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DataAccessLayer.Repository;
+import DataAccessLayer.DTOs.CategoryDTO;
+
+public class CategoryDAO extends DAO<CategoryDTO> {
+
+    public CategoryDAO(){
+        super("Categories");
+    }
+
+    @Override
+    public CategoryDTO makeDTO(ResultSet rs) throws SQLException {
+        if (!rs.next())
+            throw new SQLException("Can't make DTO from nothing!");
+
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String categoryParentId = rs.getString("parent");
+        CategoryDTO parenCategoryDTO;
+        if(categoryParentId == null){
+            parenCategoryDTO = null;
+        }else{
+            parenCategoryDTO = getById(Integer.parseInt(categoryParentId));
+        }
+        return new CategoryDTO(id, name, parenCategoryDTO);
+    }
+
+    public CategoryDTO getById(int categoryId) throws SQLException {
+        Connection con = Repository.getInstance().connect();
+
+        String query = "SELECT * FROM Categories WHERE id= ?;";
+        PreparedStatement statement = con.prepareStatement(query);
+        statement.setInt(1, categoryId);
+        ResultSet catId = statement.executeQuery();
+
+        return makeDTO(catId);
+    }
+    
+}
--- dev/DataAccessLayer/DTOs/CategoryDTO.java
+++ dev/DataAccessLayer/DTOs/CategoryDTO.java
@@ -1,12 +1,46 @@
 package DataAccessLayer.DTOs;
 
-public class CategoryDTO {
+import java.util.HashMap;
+import java.util.Map;
+
+public class CategoryDTO implements DTO {
     private int id;
     private String name;
-    private Integer parentId; //can be null
-    private int childId; //can be null
+    private CategoryDTO parentCategoryDTO; //can be null
 
-    public CategoryDTO(int id, String name, Integer parentId){
-        
+    public CategoryDTO(int id, String name, CategoryDTO parentCategoryDTO){
+        this.id = id;
+        this.name = name;
+        this.parentCategoryDTO = parentCategoryDTO;
     }
+
+    @Override
+    public Map<String, String> getNameToVal() {
+        Map<String, String> nameToVal = new HashMap<>();
+        nameToVal.put("id", "" + id);
+        nameToVal.put("name", ""+ name);
+        nameToVal.put("parent", ""+ getParentId());
+        return nameToVal;
+    }
+
+    public Integer getParentId() {
+        if(parentCategoryDTO==null){
+            return null;
+        }else{
+            return parentCategoryDTO.getId();
+        }
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CategoryDTO getParentCategoryDTO(){
+        return parentCategoryDTO;
+    }
+
+
 }

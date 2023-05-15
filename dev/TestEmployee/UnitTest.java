4033f627525ef7026f2bb62ed268068744d0f957
kfirShal <kfirshal@post.bgu.ac.il>
2023-05-15 23:11:59 +0300

    tests
    tests

--- dev/TestEmployee/UnitTest.java
+++ dev/TestEmployee/UnitTest.java
@@ -1,21 +1,56 @@
 package TestEmployee;
 
-import BussinessLayer.TransPortLayer.Destination;
-import BussinessLayer.TransPortLayer.Transport;
-import BussinessLayer.TransPortLayer.TransportFacade;
+import BussinessLayer.TransPortLayer.*;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class UnitTest {
 
     //private DriverFacade driverFacade;
+    private TruckFacade truckFacade;
     
 
     @BeforeEach
     public void setUp() {
+
         //driverFacade = new DriverFacade();
+        truckFacade = new TruckFacade();
+        truckFacade.newTrack("aaaa", "B", 200, 250);
        // driverFacade.getDrivers().put(1, new Driver(1, "rotem", "ABC123"));
         //driverFacade.getDrivers().put(2, new Driver(2, "kfir", "DEF456"));
     }
-    /* 
+
+    @Test
+    public void testTruck() {
+        truckFacade.getTruck("aaaa").setModel("A");
+        assertEquals("don't change the model of track", "A" ,truckFacade.getTruck("aaaa").getModel());
+    }
+
+    @Test
+    public void testWightTruck() {
+        truckFacade.getTruck("aaaa").setWeightMax(400);
+        assertEquals("don't change the weight max", 400, truckFacade.getTruck("aaaa").getWeightMax());
+    }
+
+    @Test
+    public void testWightNetoTruck() {
+        truckFacade.getTruck("aaaa").setWeightNeto(600);
+        assertEquals("don't change the weight max", 600, truckFacade.getTruck("aaaa").getWeightMax());
+    }
+
+    @Test
+    public void testWightTruckTwo() {
+        truckFacade.getTruck("aaaa").setWeightMax(0);
+        assertEquals("don't change the weight max", 0, truckFacade.getTruck("aaaa").getWeightMax());
+    }
+/*
 
     @Test
     public void testDriverExist() {
@@ -114,9 +149,9 @@ public class UnitTest {
     public void testRemoveTransport() {
         // Create a new transport and add it to the facade
         TransportFacade transportFacade = TransportFacade.getInstance();
-        Transport transport = new Transport(1, "2023-04-01", "12:00", "TR-123", "John Doe", 1, "Factory A",
-                new ArrayList<Destination>(), new ArrayList<Delivery>(), 5000, 10000);
-        transportFacade.addTransport(transport.getId(),transport);
+        //Transport transport = new Transport(1, "2023-04-01", "12:00", "TR-123", "John Doe", 1, "Factory A",
+           //     new ArrayList<Destination>(), new ArrayList<Delivery>(), 5000, 10000);
+        //transportFacade.addTransport(transport.getId(),transport);
 
         // Ensure the transport was added
         assertTrue(transportFacade.getTransportMap().containsKey(1));
@@ -128,61 +163,62 @@ public class UnitTest {
         assertFalse(transportFacade.getTransportMap().containsKey(1));
     }
 
+    /*
     @Test
     public void testChangingOrderValidInput() {
         // Create a transport object with destinations
-        Transport transport = new Transport(1, "2023-04-01", "10:00", "TRUCK001", "John Doe", 1, "Los Angeles",
+        //Transport transport = new Transport(1, "2023-04-01", "10:00", "TRUCK001", "John Doe", 1, "Los Angeles",
                 Arrays.asList(
                         new Destination("San Francisco", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
                         new Destination("Sacramento", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
                         new Destination("San Diego", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE)
-                ),
-                new ArrayList<Delivery>(), 5000, 10000);
+               // ),
+          //      new ArrayList<Delivery>(), 5000, 10000);
 
         // Redirect system input stream to provide input to the scanner
-        ByteArrayInputStream in = new ByteArrayInputStream("1,3,2\n".getBytes());
-        System.setIn(in);
-        Scanner scanner = new Scanner(System.in);
+      //  ByteArrayInputStream in = new ByteArrayInputStream("1,3,2\n".getBytes());
+       // System.setIn(in);
+       // Scanner scanner = new Scanner(System.in);
 
         // Call the changingOrder method
-        TransportFacade.getInstance().changingOrder(transport);
+       // TransportFacade.getInstance().changingOrder(transport);
 
         // Verify that the order of destinations has been updated correctly
-        List<Destination> expected = Arrays.asList(
-                new Destination("San Francisco", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
-                new Destination("Sacramento", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
-                new Destination("San Diego", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE)
-        );
-        List<Destination> actual = transport.getDestinationList();
-        assertEquals(expected, actual);
+     //   List<Destination> expected = Arrays.asList(
+            //    new Destination("San Francisco", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
+          //      new Destination("Sacramento", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
+         //       new Destination("San Diego", "123-456-7890", "Jane Smith", Location.CENTER, DestinationType.SOURCE),
+       // );
+       // List<Destination> actual = transport.getDestinationList();
+       // assertEquals(expected, actual);
     }
 
     @Test
     public void testChangingOrderWithInvalidInput() {
-        // Create a transport with a list of destinations
+        // Create transport with a list of destinations
         List<Destination> destinationList = new ArrayList<>();
         destinationList.add(new Destination("123 Main St.", "555-1234", "John Smith", Location.CENTER, DestinationType.SOURCE));
         destinationList.add(new Destination("456 Elm St.", "555-5678", "Jane Doe", Location.CENTER, DestinationType.SOURCE));
-        Transport transport = new Transport(1, "2023-04-01", "10:00", "ABC123", "John Doe", 1, "789 Oak St.", destinationList, new ArrayList<Delivery>(), 1000, 5000);
+        //Transport transport = new Transport(1, "2023-04-01", "10:00", "ABC123", "John Doe", 1, "789 Oak St.", destinationList, new ArrayList<Delivery>(), 1000, 5000);
 
         // Create a mock Scanner object with invalid input
         Scanner scanner = new Scanner("1, 3, 5, abc\n");
 
         // Set the System.in to use the mock Scanner object
-        InputStream inputStream = System.in;
-        System.setIn(inputStream);
+        //InputStream inputStream = System.in;
+        //System.setIn(inputStream);
 
         // Call the changingOrder method with the mock Scanner object
         TransportFacade transportFacade = TransportFacade.getInstance();
-        transportFacade.changingOrder(transport);
+        //transportFacade.changingOrder(transport);
 
         // Check that the transport's list of destinations has not been changed
-        assertEquals(destinationList, transport.getDestinationList());
+        //assertEquals(destinationList, transport.getDestinationList());
     }
 
 
 
-
+*/
 
 
 

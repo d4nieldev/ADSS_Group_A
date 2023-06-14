e9d99f37412cab55458b11ab4ab7db0fba14662c
BacharMetar <127039537+BacharMetar@users.noreply.github.com>
2023-06-14 18:47:50 +0300

    done with add new product

--- dev/PersentationLayer/GUI/Tasks/AddNewProductFrame.java
+++ dev/PersentationLayer/GUI/Tasks/AddNewProductFrame.java
@@ -44,21 +44,27 @@ public class AddNewProductFrame implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 550);
         frame.setLocationRelativeTo(null); // Center the frame on the screen
+
         invalidInputLabel = new JLabel();
         invalidInputLabel.setVisible(false);
 
         // Create the top panel with BorderLayout
         JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.setBackground(new Color(132, 136, 168)); // Apple blue color
 
-        // Create the back button
         backButton = new JButton("Back");
         backButton.addActionListener(this);
-
+        backButton.setBackground(new Color(0, 122, 255)); // Apple blue color
+        backButton.setForeground(Color.WHITE);
+        backButton.setBorderPainted(false); // Remove button border
+        backButton.setFocusPainted(false); // Remove button focus border
         topPanel.add(backButton, BorderLayout.WEST);
 
         // Create the label
         JLabel label = new JLabel("Add new Product");
         label.setHorizontalAlignment(JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 24));
+        label.setForeground(Color.WHITE);
         topPanel.add(label, BorderLayout.CENTER);
 
         // Create the main panel for text boxes
@@ -70,43 +76,33 @@ public class AddNewProductFrame implements ActionListener {
         // Create the text boxes with labels
         JLabel branchIdLabel = new JLabel("Branch ID");
         branchIdTextField = new JTextField(20);
-        branchIdTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
-
 
         JLabel productCodeLabel = new JLabel("Product Code");
         productCodeTextField = new JTextField(20);
-        productCodeTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
 
         JLabel discountValueLabel = new JLabel("Discount Value");
         discountValueTextField = new JTextField(20);
-        discountValueTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size1
 
         JLabel discountStartLabel = new JLabel("Discount Start Date");
         discountStartTextField = new JTextField(20);
-        discountValueTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
         discountStartTextField.setText("2024-11-11");
 
         JLabel discountEndLabel = new JLabel("Discount End Date");
         discountEndTextField = new JTextField(20);
-        discountEndTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
         discountEndTextField.setText("2024-11-11");
-
 
         JLabel priceLabel = new JLabel("Price");
         priceTextField = new JTextField(20);
-        priceTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
 
         JLabel minQuantityLabel = new JLabel("Min Quantity");
         minQuantityTextField = new JTextField(20);
-        minQuantityTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
 
         JLabel idealQuantityLabel = new JLabel("Ideal Quantity");
         idealQuantityTextField = new JTextField(20);
-        idealQuantityTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
 
         // Create the discount type combo box
         JLabel discountTypeLabel = new JLabel("Discount Type");
-        discountTypeComboBox = new JComboBox<>(new String[]{"DiscountPercentage", "DiscountFix"});
+        discountTypeComboBox = new JComboBox<>(new String[]{"Discount Percentage", "Discount Fix"});
 
         // Add the labels and text fields to the main panel
         gbc.gridx = 0;
@@ -173,7 +169,6 @@ public class AddNewProductFrame implements ActionListener {
         gbc.gridy = 7;
         mainPanel.add(idealQuantityTextField, gbc);
 
-
         // Add the discount type label and combo box to the main panel
         gbc.gridx = 0;
         gbc.gridy = 8;
@@ -183,9 +178,11 @@ public class AddNewProductFrame implements ActionListener {
         gbc.gridy = 8;
         mainPanel.add(discountTypeComboBox, gbc);
 
+
         // Create the submit button
         submitButton = new JButton("Submit");
         submitButton.addActionListener(this);
+        ////
         gbc.gridx = 1;
         gbc.gridy = 9;
         gbc.anchor = GridBagConstraints.EAST; // Align the button to the right
@@ -201,9 +198,9 @@ public class AddNewProductFrame implements ActionListener {
         gbc.anchor = GridBagConstraints.CENTER;
         mainPanel.add(invalidInputLabel, gbc);
 
-
         // Add the top panel and main panel to the frame's content pane
         frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(Color.WHITE);
         frame.getContentPane().add(topPanel, BorderLayout.NORTH);
         frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
 

2e203a30fafd5dd5fae22364578f02e6a52535f4
inbarbenchaim <117454266+inbarbenchaim@users.noreply.github.com>
2023-06-17 20:26:53 +0300

    updates litle bbugs with tal

--- dev/PresentationLayer/GUI/GUI.java
+++ dev/PresentationLayer/GUI/GUI.java
@@ -6,8 +6,10 @@ public class GUI {
     public static void activate(String role, ServiceFactory serviceFactory) {
         if (role.equals("StoreManager"))
             new StoreManagerScreen();
-        else if (role.equals("HRManager"))
+        else if (role.equals("HRManager")){
+            serviceFactory.getEmployeeService().logIn(123456789, "HRmanager");
             new HRManagerScreen(serviceFactory);
+        }
         else
             throw new IllegalArgumentException("Unknown role: " + role);
     }

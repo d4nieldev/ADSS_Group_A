4ed1d790a904b7c52e838ec2de4db914f303cbe1
BacharMetar <127039537+BacharMetar@users.noreply.github.com>
2023-06-18 22:24:12 +0300

    .

--- dev/PersentationLayer/Program.java
+++ dev/PersentationLayer/Program.java
@@ -5,15 +5,15 @@ import PersentationLayer.GUI.GUI;
 
 public class Program {
     public static void main(String[] args) {
-        if (args.length != 2)
-            throw new IllegalArgumentException("Expected 2 arguments: mode and role");
-
-        String mode = args[0];
-        String role = args[1];
-
-        if (mode.equals("CLI"))
-            CLI.activate(role);
-        else
-            GUI.activate(role);
+//        if (args.length != 2)
+//            throw new IllegalArgumentException("Expected 2 arguments: mode and role");
+//
+//        String mode = args[0];
+//        String role = args[1];
+//
+//        if (mode.equals("CLI"))
+//            CLI.activate(role);
+//        else
+            GUI.activate("StoreManager");
     }
 }

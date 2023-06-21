a100c9b151b52b23d196a41328f5902b127891ad
TalKoren11 <kotal@post.bgu.ac.il>
2023-06-21 20:17:23 +0300

    Submit!!

--- dev/PresentationLayer/Program.java
+++ dev/PresentationLayer/Program.java
@@ -8,9 +8,9 @@ public class Program {
     private static ServiceFactory serviceFactory = new ServiceFactory();
 
     public static void main(String[] args) {
-        args = new String[2];
-        args[0] = "GUI";
-        args[1] = "StoreManager";
+//        args = new String[2];
+//        args[0] = "GUI";
+//        args[1] = "StoreManager";
         if (args.length != 2)
             throw new IllegalArgumentException("Expected 2 arguments: mode and role");
 
@@ -21,7 +21,9 @@ public class Program {
 
         if (mode.equals("CLI"))
             CLI.activate(role, serviceFactory);
-        else
+        else if (mode.equals("GUI"))
             GUI.activate(role, serviceFactory);
+        else
+            throw new Error("Not Allowed Mode.");
     }
 }
